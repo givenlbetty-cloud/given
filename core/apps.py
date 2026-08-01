cfb7a2acfbde45b019b9ca43ec56980b769be4c2
ATJ Admin <admin@atj.com>
2026-08-01 11:40:52 +0200

    FIX_V4: Site id=1 dans core/apps.py ready() - point d'entree Django standard, fiable avec gunicorn

--- core/apps.py
+++ core/apps.py
@@ -2,4 +2,17 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = "core"
+
+    def ready(self):
+        # Garantit que le Site (id=1) existe pour django.contrib.sites + allauth
+        # AppConfig.ready() est le point d'entrée standard Django, fiable avec gunicorn
+        from django.contrib.sites.models import Site
+        Site.objects.get_or_create(
+            id=1,
+            defaults={
+                'domain': 'atj-beta.onrender.com',
+                'name': 'ATJ',
+            }
+        )
