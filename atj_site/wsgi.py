474e6554f1469196c9db229aefa1fd7bf235ade3
ATJ Admin <admin@atj.com>
2026-08-03 11:10:50 +0200

    feat: populate_formations auto au démarrage gunicorn

--- atj_site/wsgi.py
+++ atj_site/wsgi.py
@@ -6,5 +6,15 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atj_site.settings")
 
+import django
+django.setup()
+
+# Repeupler automatiquement les formations au démarrage (Render)
+from django.core.management import call_command
+try:
+    call_command('populate_formations')
+except Exception:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
