d4e7139c4f29133c35b41bcc35f8783fa08ee0e6
givenlbetty-cloud <givenlbetty@gmail.com>
2026-09-21 15:15:40 +0200

    correction de la base de donnees avec parse

--- atj_site/settings.py
+++ atj_site/settings.py
@@ -108,11 +108,12 @@ if db_url:
     }
 else:
     # Base de données par défaut si DATABASE_URL n'est pas définie (évite de planter au build)
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+   DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=True
+    )
     }
 
 AUTH_PASSWORD_VALIDATORS = [
