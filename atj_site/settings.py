7bc9fd05405d6aeae1939ce1f7926ceacc718bf2
givenlbetty-cloud <givenlbetty@gmail.com>
2026-09-22 13:45:30 +0200

    Update settings.py

--- atj_site/settings.py
+++ atj_site/settings.py
@@ -300,6 +300,6 @@ STORAGES = {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
     "staticfiles": {
-        "BACKEND": "django.congit add .trib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
